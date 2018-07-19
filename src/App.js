69a9045ecad1687c9bfa3c8ce3cebc5dfc2ceaae
Athoof <athoof@gmail.com>
2018-07-20 00:12:07 +0500

    FAQ segment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
+import { Container, Menu, Responsive, Image, Segment } from 'semantic-ui-react';
 import './App.css';
-import { Menu, Container, Responsive } from 'semantic-ui-react';
+import logo from './navaanavai.svg';
 
 const menuItems = [
   { key: 'home', active: true, name: "Home" },
@@ -9,13 +10,18 @@ const menuItems = [
   { key: 'contact', name: "Contact" },
 ]
 
+const menu = () => {
+  <Responsive as={Menu} inverted color={'red'} items={menuItems} fluid />
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Responsive className="main" as={Container} fluid >
-        <Responsive as={Menu} inverted color={'red'} items={menuItems} fluid />
-      </Responsive>
+      <Responsive as={Container} className="main" fluid >
+        <Image className='logo' src={logo} centered />
+        </Responsive>
+      <Responsive as={Container} className='faq' fluid><Segment vertical secondary inverted color={'red'} as='h1'>Who are we?</Segment></Responsive>
       </div>
     );
   }
