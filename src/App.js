53ca74f3b8d1caff0e7181d0b2454d3dd3523659
Athoof <athoof@gmail.com>
2018-07-20 13:31:53 +0500

    Single page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Menu, Responsive, Image, Segment, Button, Icon} from 'semantic-ui-react';
+import { Container, Menu, Responsive, Image, Segment, Button, Icon, Card, Divider} from 'semantic-ui-react';
 import './App.css';
 import logo from './navaanavai.svg';
 import { FullPage, Slide } from 'react-full-page';
+// import faq from './faq.md';
 
 const menuItems = [
   { key: 'home', active: true, name: "Home" },
@@ -11,35 +12,34 @@ const menuItems = [
   // { key: 'contact', name: "Contact" },
 ]
 
+var topMenu = () => {
+  return(
+    <Responsive as={Menu} inverted color={'red'} items={menuItems} fluid />
+  )
+}
+
 var mainPage = () => {
   return(
-    <Responsive as={Container} className="main" >
-      <Responsive as={Menu} inverted color={'red'} items={menuItems} fluid />              
-      <Image className='logo' src={logo} centered />
-      <Button color='twitter'>
-        <Icon name='twitter' /> Twitter
-      </Button>
-      <Button color='facebook'>
-        <Icon name='facebook' /> Facebook
-      </Button>
-    </Responsive>
+    <Image className='logo' src={logo} centered />
   );
 }
 
+var socialButtons = () => {
+  return(
+    <Responsive as={Container} className='buttonGroup' centered fluid >
+        <Button className='socialButtons' circular color='facebook' icon='facebook' />
+        <Button className='socialButtons' circular color='twitter' icon='twitter' />
+        <Button className='socialButtons' circular color='google plus' icon='mail' />
+    </Responsive>
+  )
+}
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <FullPage duration={1000}>
-          <Slide className='dark'>
-            {mainPage()}
-          </Slide>
-          <Slide className='light'>
-            <Responsive as={Container} className='faq'>
-              <Segment>Who are we?</Segment>
-            </Responsive>
-          </Slide>
-        </FullPage>
+      <div className="main dark">
+          {mainPage()}
+          <Divider clearing inverted />
+          {socialButtons()}
       </div>
     );
   }
